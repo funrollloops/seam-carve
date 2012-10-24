2cad3205cff454e6227b3cfb1d6da468e6329a09
Sagar Mittal <sagar@sagarmittal.com>
2012-10-23 22:44:07 -0700

    Use neighborhood variance as the energy function

--- energy.go
+++ energy.go
@@ -3,6 +3,7 @@ package enhance
 import (
   "image"
   "image/color"
+  "math"
 )
 
 type RGBA struct {
@@ -26,12 +27,51 @@ func rgba(r, g, b, a uint32) *RGBA {
 
 func Energy(in image.Image) *image.Gray16 {
   bounds := in.Bounds()
+  energyAt := func(minx, maxx, miny, maxy int) float32 {
+    var avgR, avgG, avgB, avgA uint32
+    var count uint32
+    for x := minx; x <= maxx; x++ {
+      for y := miny; y <= maxy; y++ {
+        r, g, b, a := in.At(x, y).RGBA()
+        count++
+        avgR += r
+        avgG += g
+        avgB += b
+        avgA += a
+      }
+    }
+    avgR /= count
+    avgG /= count
+    avgB /= count
+    avgA /= count
+
+    var variance float64
+    sqdiff := func(a, b uint32) float64 { d:= a - b; return float64(d * d); }
+    for x := minx; x <= maxx; x++ {
+      for y := miny; y <= maxy; y++ {
+        r, g, b, a := in.At(x, y).RGBA()
+        variance += sqdiff(avgR, r) + sqdiff(avgG, g) + sqdiff(avgB, b) + sqdiff(avgA, a)
+      }
+    }
+    return float32(math.Sqrt(variance))
+  }
+
+  var max_energy float32
+  as_float := make([][]float32, bounds.Size().Y)
+  for y := bounds.Min.Y + 1; y < bounds.Max.Y - 1; y++ {
+    row := make([]float32, bounds.Size().X)
+    as_float[y - bounds.Min.Y] = row
+    for x := bounds.Min.X + 1; x < bounds.Max.X - 1; x++ {
+      e := energyAt(x - 1, x + 1, y - 1, y + 1)
+      if e > max_energy { max_energy = e }
+      row[x - bounds.Min.X] = e
+    }
+  }
+
   energy := image.NewGray16(bounds)
-  for x := bounds.Min.X + 1; x < bounds.Max.X - 1; x++ {
-    for y := bounds.Min.Y + 1; y < bounds.Max.Y - 1; y++ {
-      dx := rgba(in.At(x - 1, y).RGBA()).Minus(rgba(in.At(x + 1, y).RGBA()))
-      dy := rgba(in.At(x, y - 1).RGBA()).Minus(rgba(in.At(x, y + 1).RGBA()))
-      energy.SetGray16(x, y, color.Gray16{uint16((dx + dy) / 2)})
+  for y, row := range as_float {
+    for x, e := range row {
+      energy.SetGray16(x, y, color.Gray16{uint16(float32((1<<16) - 1) * e / max_energy)})
     }
   }
   return energy
